e69584e8b68b7564fea07b7de341976de883bc5d
Chao Li <cli49003@usc.edu>
2022-04-06 23:01:12 -0700

    Finished ConvertTimeIndexToHourTest and DatePickerTest

--- app/src/test/java/com/life/hacker/uscrecapp/UtilTest/ConvertTimeIndexToHourTest.java
+++ app/src/test/java/com/life/hacker/uscrecapp/UtilTest/ConvertTimeIndexToHourTest.java
@@ -1,4 +1,27 @@
 package com.life.hacker.uscrecapp.UtilTest;
 
+import static org.junit.Assert.assertEquals;
+
+import com.life.hacker.uscrecapp.Util;
+
+import org.junit.Test;
+
 public class ConvertTimeIndexToHourTest {
+    @Test
+    public void timeIndexFromMorningTo12() {
+        assertEquals("8:00 - 9:00", Util.convertTimeIndexToHour(8));
+        assertEquals("9:00 - 10:00", Util.convertTimeIndexToHour(9));
+        assertEquals("10:00 - 11:00", Util.convertTimeIndexToHour(10));
+        assertEquals("11:00 - 12:00", Util.convertTimeIndexToHour(11));
+        assertEquals("12:00 - 13:00", Util.convertTimeIndexToHour(12));
+    }
+
+    @Test
+    public void timeIndexAfter12() {
+        assertEquals("13:00 - 14:00", Util.convertTimeIndexToHour(13));
+        assertEquals("14:00 - 15:00", Util.convertTimeIndexToHour(14));
+        assertEquals("15:00 - 16:00", Util.convertTimeIndexToHour(15));
+        assertEquals("16:00 - 17:00", Util.convertTimeIndexToHour(16));
+        assertEquals("17:00 - 18:00", Util.convertTimeIndexToHour(17));
+    }
 }
--- app/src/test/java/com/life/hacker/uscrecapp/UtilTest/DatePickerTest.java
+++ app/src/test/java/com/life/hacker/uscrecapp/UtilTest/DatePickerTest.java
@@ -1,4 +1,52 @@
 package com.life.hacker.uscrecapp.UtilTest;
 
+import static org.junit.Assert.assertEquals;
+
+import com.life.hacker.uscrecapp.Util;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class DatePickerTest {
+    @Test
+    public void findNextDayCommonTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2022, 10, 15, 10, 59, 59);
+        assertEquals("2022-11-16", Util.formatDateToStardard(Util.findNextDay(calendar.getTime())));
+        calendar.set(2019, 1, 1, 9, 30, 27);
+        assertEquals("2019-02-02", Util.formatDateToStardard(Util.findNextDay(calendar.getTime())));
+    }
+
+    @Test
+    public void findPrevDayCommonTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2022, 10, 15, 10, 59, 59);
+        assertEquals("2022-11-14", Util.formatDateToStardard(Util.findPrevDay(calendar.getTime())));
+        calendar.set(2019, 1, 2, 9, 30, 27);
+        assertEquals("2019-02-01", Util.formatDateToStardard(Util.findPrevDay(calendar.getTime())));
+    }
+
+    @Test
+    public void findNextDayEdgeTest() {
+        // Month Boundary
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2022, 10, 30, 10, 59, 59);
+        assertEquals("2022-12-01", Util.formatDateToStardard(Util.findNextDay(calendar.getTime())));
+        // Year Boundary
+        calendar.set(2019, 11, 31, 9, 30, 27);
+        assertEquals("2020-01-01", Util.formatDateToStardard(Util.findNextDay(calendar.getTime())));
+    }
+
+    @Test
+    public void findPrevDayEdgeTest() {
+        // Month Boundary
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2022, 10, 1, 10, 59, 59);
+        assertEquals("2022-10-31", Util.formatDateToStardard(Util.findPrevDay(calendar.getTime())));
+        // Year Boundary
+        calendar.set(2019, 0, 1, 9, 30, 27);
+        assertEquals("2018-12-31", Util.formatDateToStardard(Util.findPrevDay(calendar.getTime())));
+    }
 }
