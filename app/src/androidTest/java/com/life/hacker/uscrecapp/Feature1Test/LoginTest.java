7b4d7be847347d09d261a221d1d46f2903b98526
Kevin Che <cheyuany@usc.edu>
2022-04-10 16:27:12 -0700

    work in progress login test, as an example

--- app/src/androidTest/java/com/life/hacker/uscrecapp/Feature1Test/LoginTest.java
+++ app/src/androidTest/java/com/life/hacker/uscrecapp/Feature1Test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.life.hacker.uscrecapp.Feature1Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -8,29 +9,38 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.life.hacker.uscrecapp.R;
 import com.life.hacker.uscrecapp.activity.LoginActivity;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import kotlin.jvm.JvmField;
-
 @RunWith(AndroidJUnit4.class)
 public class LoginTest {
+    //This denotes the starting activity
     @Rule
-    @JvmField
     public ActivityScenarioRule<LoginActivity> loginActivityScenarioRule = new ActivityScenarioRule<>(LoginActivity.class);
-    //public ActivityScenario<LoginActivity>
 
+    private void tryLogout() {
+        try {
+            onView(withId(R.id.mapButtonLogout)).check(matches(isDisplayed()));
+            //if we are in map view, means we've login already
+            onView(withId(R.id.mapButtonLogout)).perform(click());
+        } catch (NoMatchingViewException e) {
+            //we are in login page
+        }
+    }
 
     @Before
     public void setUp() {
+        tryLogout();
         //Check everything is there
         onView(withId(R.id.loginEditTextEmail)).check(matches(isDisplayed()));
         onView(withId(R.id.loginEditTextPassword)).check(matches(isDisplayed()));
@@ -38,19 +48,30 @@ public class LoginTest {
         onView(withId(R.id.loginButtonToSignUp)).check(matches(isDisplayed()));
     }
 
+    @After
+    public void after() {
+        tryLogout();
+    }
+
+
     @Test
     public void loginTestEmptyEmail() {
         onView(withId(R.id.loginButtonLogin)).perform(click());
-        onView(withId(R.id.loginTextViewErrorMessage)).check(matches(withText("Invalid Email Address")));
+        onView(withId(R.id.loginTextViewErrorMessage)).check(matches(withText(R.string.loginInvalidEmail)));
     }
 
     @Test
     public void loginTestEmptyPassword() {
         onView(withId(R.id.loginEditTextEmail)).perform(typeText("test@usc.edu"));
-
-        onView(withId(R.id.loginTextViewErrorMessage)).check(matches(withText("Invalid Password Address")));
+        onView(withId(R.id.loginTextViewErrorMessage)).check(matches(withText(R.string.loginInvalidPassword)));
     }
 
+    @Test
+    public void loginTestWrongEmail() {
+        onView(withId(R.id.loginEditTextEmail)).perform(typeText("test@usc.edu"));
+        onView(withId(R.id.loginEditTextPassword)).perform(typeText("wrong pw"));
+        onView(withId(R.id.loginTextViewErrorMessage)).check(matches(withText("Incorrect Email or password")));
+    }
 
     @Test
     public void loginTestSuccess() {
@@ -59,6 +80,7 @@ public class LoginTest {
 
         onView(withId(R.id.loginButtonLogin)).perform(click());
 
-        onView(withId(R.id.mapButtonGoToSummary)).check(matches(isDisplayed()));
+        pressBack();
+        //onView(withId(R.id.mapButtonLogout)).check(matches(isDisplayed()));
     }
 }
